0f8e190361dec76be2cfa447bd381063a76e0225
Halah <halah.alshaikhly@gmail.com>
2022-07-19 18:11:19 -0700

    removes react imports

--- my-scope/ui/my-welcome/my-welcome.composition.tsx
+++ my-scope/ui/my-welcome/my-welcome.composition.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { MyWelcome } from './my-welcome';
 
 export const BasicMyWelcome = () => {
--- my-scope/ui/my-welcome/my-welcome.spec.tsx
+++ my-scope/ui/my-welcome/my-welcome.spec.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import { BasicMyWelcome } from './my-welcome.composition';
 
